6265e3dd2faa05b53714749f06f153a80e475139
Geoffrey Burt <geoffreyjburt@gmail.com>
2025-04-02 16:16:25 -0400

    Added a short description message and a button that navigates to the Product Listings page on the Home Page

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,4 +1,5 @@
 import { Container, Carousel, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function HomePage() {
 
@@ -7,6 +8,11 @@ function HomePage() {
                 <Row>
                     <Col>
                         <h3>Welcome to Fake Store!</h3>
+                        <p>Discover the latest trends in fashion, electronics, and more! 
+                            <Link to="/ProductListing">
+                            <button variant="primary">Check it out here!</button>
+                            </Link>
+                        </p>
                     </Col>
                 </Row>
             </Container>
